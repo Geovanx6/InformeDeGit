b4fe7b924f1b509306ddbe98495e6be5065a661a
Geovanx6 <azambrano0417@utm.edu.ed>
2026-07-31 18:37:26 -0500

    Se utilizo map y filter

--- Funcional.py
+++ Funcional.py
@@ -1,19 +1,5 @@
 from functools import reduce
 
-def Aprobo(estudiante):
-    nombre, nota = estudiante
-    return nota >= 70
-
-
-def Nota(estudiante):
-    _, nota = estudiante
-    return nota
-
-
-def Nombre(estudiante):
-    nombre, _ = estudiante
-    return nombre
-
 estudiantes = [
     ("Ana", 95),
     ("Luis", 62),
@@ -22,3 +8,19 @@ estudiantes = [
     ("Elena", 90),
 ]
 
+def Aprobado(estudiante):
+    nombre, nota = estudiante
+    return nota >= 70
+
+def Nota(estudiante):
+    _, nota = estudiante
+    return nota
+
+def Nombre(estudiante):
+    nombre, _ = estudiante
+    return nombre
+
+aprobados = tuple(filter(Aprobado, estudiantes))
+nombres = tuple(map(Nombre, aprobados))
+print("Estudiantes aprobados:")
+print(nombres)
